93cc8ba2a76ce21241808682bfdd3d8e535992a0
Samu <samueljose.garcia@iesdonana.org>
2023-11-24 17:19:12 +0100

    Creadas las migraciones ivas, añadir columna iva a articulos y hacer que la migración que haga que la columna iva_id de la tabla artículos sea not null

---
+++ database/migrations/2023_11_24_133923_add_iva_id_to_articulos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articulos', function (Blueprint $table) {
+            $table->unsignedBigInteger('iva_id')->nullable();
+            $table->foreign('iva_id')->references('id')->on('ivas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articulos', function (Blueprint $table) {
+            $table->dropForeign(['iva_id']);
+            $table->dropColumn('iva_id');
+        });
+    }
+};
